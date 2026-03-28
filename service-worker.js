ec8ad1c3adb5e772d2e2848f250f2e58e8c316ac
yinpsw2548-maker <yinpsw2548@gmail.com>
2026-03-28 20:07:46 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,8 @@
-// Service Worker สำหรับระบบแจ้งเตือน Art Therapy Club
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
-});
-
 self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.text() : 'มีการอัปเดตสถานะการจองอุปกรณ์';
-    const options = {
-        body: data,
-        icon: 'https://cdn-icons-png.flaticon.com/512/1827/1827347.png',
-        badge: 'https://cdn-icons-png.flaticon.com/512/1827/1827347.png'
-    };
-    event.waitUntil(self.registration.showNotification('Art Therapy Club', options));
+  const options = {
+    body: event.data ? event.data.text() : 'มีการอัปเดตสถานะการจองอุปกรณ์',
+    icon: 'https://cdn-icons-png.flaticon.com/512/1827/1827347.png',
+    badge: 'https://cdn-icons-png.flaticon.com/512/1827/1827347.png'
+  };
+  event.waitUntil(self.registration.showNotification('Art Therapy Club', options));
 });
